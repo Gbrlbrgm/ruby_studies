137d3efef81dc2111548560c24e69628f649430e
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 20:33:29 -0300

    study: how blocks work

---
+++ 07 - Blocks/introduction_to_blocks_with_the_times_method.rb
@@ -0,0 +1,9 @@
+# Argument - object, concrete value
+# Blocks - a procedure, collection of code
+
+8.times { puts "Ruby is Cool!" }
+
+3.times do
+  puts "Ruby is Awesome!"
+  puts "I'm having fun learning it!"
+end
