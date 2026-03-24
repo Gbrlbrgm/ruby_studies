b457d448c86991f0218340813bd04442df0cad28
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 13:20:54 -0300

    study: basic arithmetic in ruby

---
+++ 02 - Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,17 @@
+puts 1 + 4
+puts -6 + 13
+puts 10 - 6
+puts 3 * 4
+puts 3 * 4 * 5
+
+# Ruby always floor divisions
+puts 10 / 5
+puts 12 / 5
+
+# However, this won't floor
+puts 12.0 / 5
+puts 12 / 5.0
+puts 12.0 / 5.0
+
+# Module
+puts 12 % 5 
