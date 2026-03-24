b5683b17fbe95a29a9277e0be410247a08aa220a
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 12:30:38 -0300

    study: how escape characters works

---
+++ 02 - Getting Started/escape_characters.rb
@@ -0,0 +1,8 @@
+puts "some text\nmore text"
+puts "\tonce upon a time"
+
+puts "juliet said \"goodbye\" to romeo"
+puts 'juliet said \'goodbye\' to romeo'
+
+puts "juliet said 'goodbye' to romeo"
+puts 'juliet said "goodbye" to romeo'
