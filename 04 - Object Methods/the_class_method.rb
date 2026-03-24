f486fb604354bdc8fc8edd0a52488df59903af7b
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 15:25:38 -0300

    study: how to class method works

---
+++ 04 - Object Methods/the_class_method.rb
@@ -0,0 +1,12 @@
+# Class - A blueprint for an object (template/schematic)
+
+# Object - An "Instance" of the class
+
+puts "hello".class
+puts "ruby is cool!".class
+
+puts 5.class
+puts -2.class
+
+puts 3.14.class
+puts 0.23.class
