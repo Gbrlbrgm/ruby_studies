503d0c1447deb3a6c66585394653ff38e89a7dda
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 13:49:55 -0300

    study: how to declare variables in ruby

---
+++ 03 - Variables/declaring_variables.rb
@@ -0,0 +1,18 @@
+age = 23
+puts age
+
+puts name = "gabriel"
+puts last_name = "bergamo"
+puts name
+puts last_name
+
+puts age + 8 # 23 + 8 = 31
+puts name + " " + last_name
+
+age = age + 1 # rewriting age
+puts age
+
+age_in_ten_years = age + 10
+puts age_in_ten_years
+
+
