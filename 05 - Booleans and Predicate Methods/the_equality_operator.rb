53a82e5d52759a3ac3f0794ff3edb463602c80ce
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 15:53:27 -0300

    study: how equality operator works

---
+++ 05 - Booleans and Predicate Methods/the_equality_operator.rb
@@ -0,0 +1,30 @@
+puts 10 == 10
+puts 2 == 3
+
+a = 10
+b = 5
+c = 10
+
+puts a == b
+puts a == c
+
+puts
+
+puts "hello" == "Hello"
+puts "hello" == "bicycle"
+puts "hello" == "hello"
+puts "hello" == "hello "
+
+puts
+
+puts "5" == "5"
+puts "5" == 5
+puts "5".to_i == 5
+puts "5" == 5.to_s
+
+puts
+
+puts 5.class
+puts 5.0.class
+
+puts 5 == 5.0
