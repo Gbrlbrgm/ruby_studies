17284152b83acd73c4dc27de6be97acc7784f926
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 20:55:28 -0300

    study: how step method works

---
+++ 07 - Blocks/the_step_method.rb
@@ -0,0 +1,15 @@
+1.step(100,5) { |count| puts count }
+# 1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76, 81, 86, 91, 96, 101
+
+1.step(50, 7) do |number|
+  puts "Let's go by 7: #{number}"
+end
+
+# Let's go by 7: 1
+# Let's go by 7: 8
+# Let's go by 7: 15
+# Let's go by 7: 22
+# Let's go by 7: 29
+# Let's go by 7: 36
+# Let's go by 7: 43
+# Let's go by 7: 50
