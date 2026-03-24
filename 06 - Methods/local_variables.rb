e286ca9aa33653f724b54337db5621bd32d01d13
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-24 18:54:20 -0300

    study: how local variable scopes and execution work

---
+++ 06 - Methods/local_variables.rb
@@ -0,0 +1,14 @@
+def film_movie
+  action_star = "Arnold Schwarzenegger"
+  puts action_star
+end
+
+film_movie
+  # puts arnold schwarzenegger
+
+action_star = "Jean Claude Van Damme"
+puts action_star
+# puts jean claude van damme
+
+film_movie # puts arnold schwarzenegger
+puts action_star # puts jean claude van damme
