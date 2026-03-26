f33a13bf7d608bc5434b94ec31f58fb3dc66903d
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-26 20:48:26 -0300

    study: access and overwrite item by index

---
+++ 12 - Arrays I/access_and_overwrite_array_element_by_index_position.rb
@@ -0,0 +1,29 @@
+fruits = ["apple", "orange", "grape", "banana"]
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[2]
+puts fruits[3]
+
+puts
+
+puts fruits[-1]
+puts fruits[-2]
+puts fruits[-3]
+puts fruits[-4]
+
+puts
+
+puts fruits.slice(0)
+puts fruits.slice(1)
+puts fruits.slice(2)
+puts fruits.slice(3)
+
+puts
+
+puts fruits.[](0)
+
+puts
+
+fruits[1] = "watermelon"
+p fruits
