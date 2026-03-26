fe603432fdaeff260536bda70a9b7076e304ebad
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-26 11:14:57 -0300

    study: using comparison in strings

---
+++ 09 - Strings I/string_comparisons.rb
@@ -0,0 +1,17 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b
+puts a == c
+puts a == a
+puts b == "hello"
+
+puts a != b
+puts a != c
+
+puts
+
+puts "A" < "B"
+puts "A" < "Z"
+puts "Z" < "a"
