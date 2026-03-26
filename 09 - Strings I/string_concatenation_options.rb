7bc0aa48d2edeec09122a7fc070c344032e587f6
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-26 11:21:38 -0300

    study: string concatenation options

---
+++ 09 - Strings I/string_concatenation_options.rb
@@ -0,0 +1,18 @@
+first_name = "Gabriel"
+last_name = "Bergamo"
+
+puts first_name + last_name
+puts last_name + first_name
+
+# first_name.concat(last_name)
+# puts first_name
+# GabrielBergamo
+
+# first_name.prepend(last_name)
+# puts first_name
+# BergamoGabriel
+
+# Shovel Operator
+a = "a"
+a << "b" << " c"
+puts a # ab c
