cad4765355e5b962f60f6d4d09a1e7e7ee806ccb
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-25 23:54:04 -0300

    study: how elsif works

---
+++ 08 - Conditionals/if_and_elsif.rb
@@ -0,0 +1,11 @@
+# elsif -> only used after if
+
+color = "blue"
+
+if color == "red"
+  puts "The color is red"
+elsif color == "blue"
+  puts "The color is blue"
+elsif color == "green"
+  puts "The color is green"
+end
