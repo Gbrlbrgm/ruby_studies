0467b295063002a2e5877542c21ac811393d7ed6
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-26 20:59:45 -0300

    study: access and overwrite with bracket syntax

---
+++ 12 - Arrays I/access_and_overwrite_multiple_array_elements_with_bracket_syntax.rb
@@ -0,0 +1,13 @@
+sesame_street = [
+  "Elmo",
+  "Big Bird",
+  "Cookie Monster",
+  "Bert",
+  "Ernie",
+  "Oscar"
+]
+
+p sesame_street[0, 3]
+
+sesame_street[3, 2] = ["Stinky", "Kermit"]
+p sesame_street
