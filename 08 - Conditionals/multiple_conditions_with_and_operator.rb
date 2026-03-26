25f2b0f5a15f3cda40dc98034322aac25f3945eb
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-25 23:58:45 -0300

    study: how multiple conditions work with and

---
+++ 08 - Conditionals/multiple_conditions_with_and_operator.rb
@@ -0,0 +1,11 @@
+# &&
+# and
+
+username = "gbrlbrgm"
+password = "123456"
+
+if username == "gbrlbrgm" && password == "123456"
+  puts "You are logged in"
+else
+  puts "Invalid username or password"
+end
