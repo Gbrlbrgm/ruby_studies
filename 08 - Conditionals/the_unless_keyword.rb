bd652b6c27f4465eb72fbd37e6223fdf676f740a
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-26 00:27:31 -0300

    study: how unless statement works

---
+++ 08 - Conditionals/the_unless_keyword.rb
@@ -0,0 +1,13 @@
+# unless
+# execute if false
+# except if = run this except if a condition is true
+
+password = "123456"
+
+# password in fact is 1234567
+
+unless password == "123456"
+  puts "Invalid password"
+else
+  puts "You are logged in"
+end
