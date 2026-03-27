158bb7791ba13cc599d1deeda24943263b5aaf96
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-27 13:55:34 -0300

    study: how include and member methods work

---
+++ 13 - Ranges/the_include__and_member__method_on_ranges.rb
@@ -0,0 +1,17 @@
+alphabet = "a".."z"
+
+puts alphabet.include?("a")
+puts alphabet.include?("z")
+puts alphabet.include?("A")
+puts alphabet.include?("Z")
+
+puts
+
+puts alphabet.member?("a")
+puts alphabet.member?("z")
+puts alphabet.member?("A")
+puts alphabet.member?("Z")
+
+puts
+
+puts alphabet === "j"
