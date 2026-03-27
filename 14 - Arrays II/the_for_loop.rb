08eab4d17871b47ae6396bc46b34c5f41e82830c
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-27 18:02:42 -0300

    study: for loop

---
+++ 14 - Arrays II/the_for_loop.rb
@@ -0,0 +1,8 @@
+# for element in iterable data structure
+
+# for number in [1, 2, 3]
+#   puts number
+# end
+
+[1, 2, 3].each { |number| puts number }
+# better than for, because for persists the variable in memory
