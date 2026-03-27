5efc2d97ff2485806c61a4996888f095ffc5bbcf
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-03-27 13:49:55 -0300

    study: how ranges work

---
+++ 13 - Ranges/intro_to_ranges.rb
@@ -0,0 +1,20 @@
+# range - sequence of numbers or letters in order
+inclusive_nums = 1..5 # closed range (5 is included)
+exclusive_nums = 1...5 # open range (5 is not included)
+
+puts inclusive_nums
+puts exclusive_nums
+
+puts inclusive_nums.first
+puts inclusive_nums.last
+
+puts exclusive_nums.first
+puts exclusive_nums.last
+
+puts 
+
+p inclusive_nums.first(3)
+p exclusive_nums.first(3)
+
+p inclusive_nums.last(3)
+p exclusive_nums.last(3)
