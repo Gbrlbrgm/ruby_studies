1a3163f22c41c23c1a55b2a002ddd52a5b163717
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-06 23:31:11 -0300

    study: how to iterate over array with while

---
+++ 14 - Arrays II/iteration_over_array_with_while_or_until_loops.rb
@@ -0,0 +1,12 @@
+animals = ["Lion", "Zebra", "Baboon", "Cheetah"]
+
+count = 0
+while count < animals.length
+    puts "The index is #{count} and the animal is #{animals[count]}"
+    count += 1
+end
+
+until i == animals.length
+    puts "The index is #{count} and the animal is #{animals[count]}"
+    count += 1
+end
