5e549a61d31f09b108e8f5e08f1af71491bb0120
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-13 20:50:02 -0300

    study: to_a and to_h methods

---
+++ 17 - Hashes/convert_hash_to_array_and_vice_versa.rb
@@ -0,0 +1,19 @@
+# to_a method
+# to_h method
+
+power_rangers = [
+  [:red, "Jason"],
+  [:black, "Zack"],
+  [:pink, "Kimberly"]
+]
+
+p power_rangers.to_h
+
+spice_girls = {
+  scary: "Melanie Brown",
+  sporty: "Melanie Chisholm",
+  baby: "Emma Bunton",
+  ginger: "Geri Halliwell"
+}
+
+p spice_girls.to_a
