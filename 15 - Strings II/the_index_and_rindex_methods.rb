9abab8a7cbeb881d7ad8f1f8151252e6a2f11be3
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-13 15:14:53 -0300

    study: how index and rindex methods work

---
+++ 15 - Strings II/the_index_and_rindex_methods.rb
@@ -0,0 +1,12 @@
+abc = "abc"
+
+# index can accept a second argument, representing the starting position
+
+p abc.index("a")
+p abc.index("b")
+p abc.index("c")
+p abc.index("d")
+
+p abc.rindex("a")
+p abc.rindex("b")
+p abc.rindex("c")
