a35ed79c75208103138e742a52ce4ec0a3df8708
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-13 18:45:28 -0300

    study: using symbols as hash keys

---
+++ 17 - Hashes/symbols_as_hash_keys.rb
@@ -0,0 +1,15 @@
+person = {
+  :name => "Gabriel",
+  :age => 23
+}
+
+puts person[:name]
+puts person[:age]
+
+person_two = {
+  name: "Gabriel",
+  age: 23
+}
+
+puts person_two[:name]
+puts person_two[:age]
