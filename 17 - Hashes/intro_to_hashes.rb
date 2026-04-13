40d53f523f88d425fd966babf878bfde05335ac7
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-13 17:26:44 -0300

    study: how hashes work

---
+++ 17 - Hashes/intro_to_hashes.rb
@@ -0,0 +1,19 @@
+# Data structure consisting of key-value pairs.
+# Key = identifier
+# Solves the problem of association
+
+# Rules of a Hash
+# Keys must be unique
+# Values can be duplicated
+# Values are extracted by key
+
+empty_hash = {}
+puts empty_hash
+puts empty_hash.class
+
+empty_array = []
+p empty_array
+p empty_array.class
+
+puts empty_hash.length
+puts empty_array.empty?
