fe9fe8fb1af7368238560c18e9c2dafd09926bca
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-27 15:45:54 -0300

    study: implicit return in blocks

---
+++ 19 - Blocks, Procs, and Lambdas/block_return_values.rb
@@ -0,0 +1,17 @@
+# Blocks implicitly return their last evaluation back to the method
+
+def who_am_i
+  puts "Hello there! Let me tell you about myself"
+  adjective = yield
+  puts "I am very #{adjective}"
+end
+
+who_am_i { "cool" }
+who_am_i { "talented" }
+
+puts
+
+who_am_i do 
+  "handsome"
+  "wonderful"
+end 
