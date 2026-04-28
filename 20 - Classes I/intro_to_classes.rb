4fc22914da054983e5bb9905bc431acd779bd688
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-04-27 23:54:48 -0300

    study: intro to classes

---
+++ 20 - Classes I/intro_to_classes.rb
@@ -0,0 +1,18 @@
+# class method - Return the class from which the object was made
+
+puts 5.class # Integer
+puts 8.class # Integer
+
+puts "hello".class # String
+puts "world".class # String 
+
+puts 3.class == 3.14.class
+
+puts 
+
+puts "Hello World".class
+puts [1, 2, 3].class
+puts ({}).class
+puts true.class
+puts false.class
+puts nil.class
