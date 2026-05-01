58064e9f73a4261ac184ea9edb2d37cc7f51729b
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-05-01 18:14:46 -0300

    study: using hashes as arguments

---
+++ 22 - Keyword Arguments/hash_as_initialize_argument.rb
@@ -0,0 +1,28 @@
+# Keyword arguments - allow invocation of a method to specify which
+# parameters the arguments correspond to
+
+class Candidate
+  attr_reader :name, :age, :occupation, :hobby, :birthplace
+  
+  def initialize(details)
+    @name = details[:name]
+    @age = details[:age]
+    @occupation = details[:occupation]
+    @hobby = details[:hobby]
+    @birthplace = details[:birthplace]
+  end
+end
+
+senator = Candidate.new(
+  name: "Mr. Smith",
+  age: 42,
+  occupation: "Engineer",
+  hobby: "Coding",
+  birthplace: "New York"
+)
+
+p senator.name 
+p senator.age
+p senator.occupation
+p senator.hobby
+p senator.birthplace
